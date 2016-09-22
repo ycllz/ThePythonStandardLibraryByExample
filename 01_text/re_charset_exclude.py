22d8db14f5e585aa7736e61392a501d93960bbf7
Herry Lee <herrymbidium@163.com>
2016-09-22 10:40:39 +0800

    1.3.4

---
+++ 01_text/re_charset_exclude.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from re_test_patterns import test_patterns
+
+test_patterns(
+    'This is some text -- with punctuation.',
+    [('[^-. ]+', 'sequences without -, ., or space'),
+     ])
---
+++ 01_text/re_test_patterns.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+def test_patterns(text, patterns=[]):
+    """Given source text and a list of patterns, look for
+    matches for each pattern within the text and print
+    them to stdout."""
+
+    # Look for each pattern in the text and print the results
+    for pattern, desc in patterns:
+        print 'Pattern %r (%s)\n' %(pattern, desc)
+        print ' %r' % text
+        for match in re.finditer(pattern, text):
+            s = match.start()
+            e = match.end()
+            substr = text[s:e]
+            n_backslashes = text[:s].count('\\')
+            prefix = '.' * (s + n_backslashes)
+            print ' %s%r' % (prefix, substr)
+
+        print
+    return
+
+if __name__ == '__main__':
+    test_patterns('abbaaabbbaaaaa', [('ab', "'a' followed by 'b'"),])
